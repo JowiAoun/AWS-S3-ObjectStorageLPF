a495966ee9a129a79df14b2ad471c70bdb6dfa66
Jowi <83415433+JowiAoun@users.noreply.github.com>
2023-06-09 22:39:54 -0400

    Implementing AWS Cognito

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const region = 'us-east-1';
 const s3 = new S3({ region });
 
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:592b675f-95c1-4596-beb8-25192a83d938',
+})
+
 function selectObject() {
     // Create a file input element
     const fileInput = document.createElement('input');
@@ -20,29 +26,29 @@ async function uploadObject(event) {
     const file = event.target.files[0];
   
     if (file) {
-      const bucket = "staticwebsite-lpf-12321";
-      const fileName = file.name;
-      const fileSize = file.size;
-      const uploadDate = new Date().toISOString();
-      const uploaderUsername = "Jowi"; // TODO: Change to user's username
+        const bucket = "staticwebsite-lpf-12321";
+        const fileName = file.name;
+        const fileSize = file.size;
+        const uploadDate = new Date().toISOString();
+        const uploaderUsername = "Jowi"; // TODO: Change to user's username
 
-      try {
-        await s3.putObject({
-        Body: file,
-        Bucket: bucket,
-        Key: fileName,
-        Metadata: {
-            "file-size": fileSize,
-            "upload-date": uploadDate,
-            "uploader-username": uploaderUsername
+        try {
+            await s3.putObject({
+                Body: file,
+                Bucket: bucket,
+                Key: fileName,
+                Metadata: {
+                    "file-size": fileSize,
+                    "upload-date": uploadDate,
+                    "uploader-username": uploaderUsername
+                }
+            });
+            console.log("Object uploaded successfully.");
+        } catch (error) {
+            console.error("Error uploading object:", error);
         }
-        });
-        console.log("Object uploaded successfully.");
-      } catch (error) {
-        console.error("Error uploading object:", error);
-      }
     }
-  }
+}
   
 
 async function getAllFilesMetadata() {
